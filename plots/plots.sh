5a50ca1e55d11685060a83b6413c3d3b3d7dd9f0
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2017-01-23 12:20:46 +0100

    unifying plot tex file

--- plots/plots.sh
+++ plots/plots.sh
@@ -1,8 +1,9 @@
 ROOT=`pwd`
-for d in `find . -type d`
+for d in `find . ! -path . -type d`
 do
     cd "$ROOT/$d"
     echo `pwd`
+    cp ../main.tex main.tex
     gnuplot plot.gnuplot
     pdflatex -interaction=batchmode main.tex
 done
